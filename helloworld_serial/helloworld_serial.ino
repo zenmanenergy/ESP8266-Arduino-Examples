ca7fd21994491d5da94e9b46a38d1ea50cc89dae
Steve Nelson <snelson@zenmanenergy.org>
2016-04-22 13:48:36 -0400

    tested with v2.2.0
    Tested this with esp8266 core v2.2.0

--- helloworld_serial/helloworld_serial.ino
+++ helloworld_serial/helloworld_serial.ino
@@ -2,7 +2,7 @@
  ESP8266 Serial Print by Steve Nelson
  This displays "Hello World" in the Serial monitor.
  
- It has been tested on an esp01 and esp12e
+ It has been tested on an esp01 and esp12e using Arduino 1.6.8 and Arduino ESP8266 core version 2.2.0
  This example code is in the public domain
  
 */
